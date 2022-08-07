448fa457fbb0d310b801c42de514fb6b46d865aa
ishizuya <102888150+ishizuya@users.noreply.github.com>
2022-08-07 10:20:53 +0000

    Layout

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,4 +1,5 @@
 class Public::HomesController < ApplicationController
   def top
+    @items = Item.all.order(release_date: :desc).limit(5)
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,12 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 require('./preview')
 
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application"
+import '@fortawesome/fontawesome-free/js/all'
+
 Rails.start()
 // Turbolinks.start()
 ActiveStorage.start()
