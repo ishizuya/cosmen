b725b16a643b097f41fcfbd8be3785ae34376b71
ishizuya <takuma3704@gmail.com>
2022-08-20 14:29:45 +0000

    fefault

--- db/migrate/20220719142718_create_items.rb
+++ db/migrate/20220719142718_create_items.rb
@@ -1,7 +1,7 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
-      t.integer :genre_id,      null: false, default: ""
+      t.integer :genre_id,      null: false
       t.string :name,           null: false, default: ""
       t.string :brand,          null: false, default: ""
       t.text :introduction,     null: false, default: ""
