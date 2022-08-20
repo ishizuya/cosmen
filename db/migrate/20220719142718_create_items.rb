23f871e8bd5c862ffa9e590510088f2f137fe554
ishizuya <takuma3704@gmail.com>
2022-08-20 14:42:34 +0000

    default

--- db/migrate/20220719142718_create_items.rb
+++ db/migrate/20220719142718_create_items.rb
@@ -5,8 +5,8 @@ class CreateItems < ActiveRecord::Migration[6.1]
       t.string :name,           null: false, default: ""
       t.string :brand,          null: false, default: ""
       t.text :introduction,     null: false
-      t.integer :price,         null: false, default: ""
-      t.integer :capacity,      null: false, default: ""
+      t.integer :price,         null: false
+      t.integer :capacity,      null: false
       t.date :release_date,     null: false, default: ""
 
       t.timestamps
